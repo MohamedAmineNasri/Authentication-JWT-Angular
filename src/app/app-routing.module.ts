f4a7af46d44a7ac1d78aee0fb4105f6a09199dc5
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2023-12-05 22:07:15 +0100

    Auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './Components/register/register.component';
 import { LoginComponent } from './Components/login/login.component';
 import { WelcomeComponent } from './Components/welcome/welcome.component';
+import {AuthGuard} from "./services/auth/auth.guard";
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'welcome', component: WelcomeComponent },
+  { path: 'welcome', component: WelcomeComponent,canActivate: [AuthGuard], },
 ];
 
 @NgModule({
